2f72d527360475fb5beb2b485c01dd2dbaf68512
bob van der staak <bob.vanderstaak@ns.nl>
2026-01-12 23:30:07 +0100

    Included check fro old edge version

--- UserAgent/UserAgentVersion.go
+++ UserAgent/UserAgentVersion.go
@@ -5,6 +5,7 @@ import "regexp"
 var (
 	oldChromeRe     = regexp.MustCompile(`Chrome/([0-9]{1,2}|1[0-3][0-9])\.[0-9]`)
 	oldFirefoxRe    = regexp.MustCompile(`Firefox/([0-9]{1,2}|1[0-3][0-9])\.[0-9]`)
+	oldEdgeRe       = regexp.MustCompile(`Edg/([0-9]{1,2}|1[0-3][0-9])\.[0-9]`)
 	oldFirefoxIOSRe = regexp.MustCompile(`FxiOS/([0-9]{1,2}|1[0-3][0-9])\.[0-9]`)
 	oldChromeIOSRe  = regexp.MustCompile(`CriOS/([0-9]{1,2}|1[0-3][0-9])\.[0-9]`)
 )
@@ -13,8 +14,13 @@ func IsOldBrowser(ua string) bool {
 	return IsOldChrome(ua) ||
 		IsOldFirefox(ua) ||
 		IsOldFirefoxIOS(ua) ||
-		IsOldChromeIOS(ua)
+		IsOldChromeIOS(ua) ||
+		IsOldEdge(ua)
 }
+func IsOldEdge(ua string) bool {
+	return oldEdgeRe.MatchString(ua)
+}
+
 func IsOldChrome(ua string) bool {
 	return oldChromeRe.MatchString(ua)
 }
